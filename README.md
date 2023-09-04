0ba24541909ec3976effe5fe4f7914fc2edc1f14
Elkinrodriguez03 <elkinrodriguez03@gmail.com>
2023-09-04 04:49:10 -0500

    Pushing local repository to Github

---
+++ README.md
@@ -0,0 +1,32 @@
+# Responsive E-commerce Website
+This project is a responsive e-commerce website built using HTML, CSS, and vanilla JavaScript. The website is designed to be fully responsive and adapt to different screen sizes.
+
+* Features
+- Responsive design that adapts to different screen sizes
+- Product carousel with prev and next button, drag functionality, infinte scroll and autoplay
+
+* Getting Started
+To get started with this project, you will need to have a basic understanding of HTML, CSS, and JavaScript. You can follow the step-by-step tutorials available on DEV Community1 to learn how to create a complete e-commerce website using these technologies.
+
+* Project Structure
+The project is structured as follows:
+
+project/
+├── index.html
+├── styles/
+│   ├── styles.css
+│   └── ...
+├── scripts/
+│   ├── main.js
+│   └── ...
+└── src/
+    ├── images/
+    └── ...
+
+The index.html file contains the main HTML structure of the website. The styles directory contains all the CSS files used to style the website, while the scripts directory contains all the JavaScript files used to add interactivity to the website. The src directory contains any additional resources used by the website, such as images.
+
+* Development
+To start developing this project, you can clone this repository to your local machine and open the index.html file in your web browser. You can then make changes to the HTML, CSS, and JavaScript files and refresh the page in your browser to see the changes.
+
+* Deployment
+To deploy this project, you can upload the entire project directory to a web server or use a static site hosting service such as GitHub Pages, Vercel, Heroku or Netlify.
---
+++ main.js
@@ -0,0 +1,143 @@
+const url = 'https://gradistore-spi.herokuapp.com/products/all';
+
+
+const content = document.getElementById('content');
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi);
+  const data = await response.json();
+  const products = data.products.nodes;
+  console.log(products);
+  return products;
+};
+
+(async () => {
+  try {
+    const allProducts = await fetchData(url);
+    let view = `
+    <div class="carousel">
+    ${allProducts.map((product) => (`
+    <div class="carousel-item">
+      <div class="image-container">
+        <button class="secondary-button">ADD TO CART</button>
+        <img src=${product.featuredImage.url} alt=${product.title} draggable="false">
+      </div>
+        <div class="product-info">
+          <h3>${product.title}</h3>
+            <div class="product-detail">
+              <div class="rate-container">
+                <div class="star">
+                  <img src="./src/images/star.png" alt="star" class="star-icon">
+                  <img src="./src/images/star.png" alt="star" class="star-icon">
+                  <img src="./src/images/star.png" alt="star" class="star-icon">
+                  <img src="./src/images/star.png" alt="star" class="star-icon">
+                  <img src="./src/images/star.png" alt="star" class="star-icon">
+                </div>
+                <p>(${product.tags[0]})</p>
+              </div>
+              <div class="price-container">
+                <p>€${product.prices.max.amount}</p>
+                <p>€${product.prices.min.amount}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    `)).join("")}
+    </div>
+  `;
+  content.innerHTML = view
+
+  const carouselContainer = document.querySelector(".carousel-container"); 
+  const carousel = document.querySelector('.carousel');
+  const leftBtn = document.querySelector(".carousel-btn-container .left-btn button");
+  const rightBtn = document.querySelector(".carousel-btn-container .right-btn button");
+  const arrowBtns = [leftBtn, rightBtn];
+  const firtsCardWidth = carousel.querySelector(".carousel-item").offsetWidth;
+  const carouselChildrens = [...carousel.children];
+
+  const rateContainer = carousel.querySelector('.rate-container .star');
+
+  let newImage = document.createElement('img');
+
+  newImage.src = './src/images/star.png';
+  newImage.alt = "star";
+  newImage.classList.add('star-icon');
+
+  rateContainer.appendChild(newImage);
+
+                  
+  let isDragging = false, startX, startScrollLeft, timeoutId;
+
+  let cardPerView = Math.round(carousel.offsetWidth / firtsCardWidth);
+                  
+  carouselChildrens.slice(0,cardPerView).reverse().forEach(item => {
+  carousel.insertAdjacentHTML("beforeend", item.outerHTML);
+  });
+
+  carouselChildrens.slice(-cardPerView).reverse().forEach(item => {
+  carousel.insertAdjacentHTML("afterbegin", item.outerHTML);
+  });
+
+  arrowBtns.forEach(btn => {
+     btn.addEventListener("click", () => {
+     carousel.scrollLeft += btn.id === "left" ? - firtsCardWidth : firtsCardWidth;
+     })
+  })
+
+  const dragging = (e) => {
+    if (!isDragging) {
+        return;
+    }
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+  };
+
+
+  const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+  };
+
+  const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging")
+  }
+
+  const autoPlay = () => {
+    if (window.innerHeight < 800) {
+        return;
+    }
+    timeoutId = setTimeout(() => carousel.scrollLeft += firtsCardWidth, 2500); 
+  }
+  autoPlay();
+
+  const infinityScroll = () => {
+    if (carousel.scrollLeft === 0) {
+         carousel.classList.add("no-transition")
+         carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+         carousel.classList.remove("no-transition")
+    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+         carousel.classList.add("no-transition")
+         carousel.scrollLeft = carousel.offsetWidth;
+         carousel.classList.remove("no-transition")
+    }
+
+    // clearTimeout(timeoutId);
+    //   if (!carouselContainer.matches(":hover")) {
+    //         autoPlay();
+    //   }
+  }
+
+  carousel.addEventListener("mousemove", dragging);
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("scroll", infinityScroll);
+  carouselContainer.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+  carouselContainer.addEventListener("mouseleave", autoPlay);
+
+} catch (error) {
+    console.error(error);
+  }
+})();
